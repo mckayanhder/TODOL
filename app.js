473d2b9726a304517643192800de4c882a619844
McKay Anhder <39319880+mckayanhder@users.noreply.github.com>
2020-08-15 19:54:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event
+todoButton.addEventListener("click", addTodo);
+
+//
